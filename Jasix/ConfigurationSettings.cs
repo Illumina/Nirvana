e6858a3bd5dd6d2a3f8228cf7321a71fe993ecde
Rajat Roy <rroy1@illumina.com>
2017-09-06 17:35:55 -0700

    bug fix

--- Jasix/ConfigurationSettings.cs
+++ Jasix/ConfigurationSettings.cs
@@ -6,7 +6,7 @@ namespace Jasix
     {
         public static string InputJson;
         public static string OutputFile;
-        public static List<string> Queries;
+        public static List<string> Queries=new List<string>();
         public static bool PrintHeader;
         public static bool PrintHeaderOnly;
         public static bool ListChromosomeName;
