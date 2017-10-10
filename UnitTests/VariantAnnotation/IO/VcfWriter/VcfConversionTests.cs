335966489d3a7b16b12dab5d76cbac97a031b8f3
Yu Jiang <yjiang2@illumina.com>
2017-10-10 14:59:56 -0700

    add unit tests for liteVcfWriter

--- UnitTests/VariantAnnotation/IO/VcfWriter/VcfConversionTests.cs
+++ UnitTests/VariantAnnotation/IO/VcfWriter/VcfConversionTests.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.Generic;
+using Moq;
 using VariantAnnotation.AnnotatedPositions;
+using VariantAnnotation.AnnotatedPositions.Transcript;
 using VariantAnnotation.Interface.AnnotatedPositions;
 using VariantAnnotation.Interface.IO;
 using VariantAnnotation.Interface.Positions;
@@ -114,10 +116,12 @@ namespace UnitTests.VariantAnnotation.IO.VcfWriter
             Assert.Equal("GMAF=G|0.002", observedVcf);
         }
 
+
+
         [Fact]
         public void RefMinor_tag_is_added_to_info_field()
         {
-            var vcfFields = "chr1	101	sa123	A	.	.	.	.".Split("\t");
+            var vcfFields = "chr1	101	sa123	A	.	.	.	.	.".Split("\t");
             var chrom = new Chromosome("chr1", "1", 0);
             var inforData = new InfoData(null, null, VariantType.SNV, null, null, null, null, null, false, null, null,
                 false, false, "", null, null);
@@ -135,15 +139,140 @@ namespace UnitTests.VariantAnnotation.IO.VcfWriter
         }
 
         [Fact]
+        public void original_updated_info_is_added_to_info_field()
+        {
+            var vcfFields = "chr1	101	sa123	A	.	.	.	.	.".Split("\t");
+            var chrom = new Chromosome("chr1", "1", 0);
+            var inforData = new InfoData(null, null, VariantType.SNV, null, null, null, null, null, false, null, null,
+                false, false, "Test=abc", null, null);
+            var position = new Position(chrom, 101, 101, "A", new[] { "." }, 100, null, null, null, inforData, vcfFields);
+            var variant = new Variant(chrom, 101, 101, "A", ".", VariantType.reference, null, true, false, null, null, new AnnotationBehavior(true, false, false, true, false, false));
+            var annotatedVariant = new AnnotatedVariant(variant);
+
+            IAnnotatedVariant[] annotatedVariants = { annotatedVariant };
+            var annotatedPosition = new AnnotatedPosition(position, annotatedVariants);
+
+            var converter = new VcfConversion();
+            var observedVcf = converter.Convert(annotatedPosition).Split("\t")[VcfCommon.InfoIndex];
+
+            Assert.Equal("Test=abc;RefMinor", observedVcf);
+        }
+
+        [Fact]
         public void OneKGAnnotation_is_handled()
         {
-            
+            var vcfFields = "chr1	101	sa123	A	T	.	.	.".Split("\t");
+            var chrom = new Chromosome("chr1", "1", 0);
+            var inforData = new InfoData(null, null, VariantType.SNV, null, null, null, null, null, false, null, null,
+                false, false, "", null, null);
+            var position = new Position(chrom, 101, 101, "A", new[] { "T" }, 100, null, null, null, inforData, vcfFields);
+            var variant = new Variant(chrom, 101, 101, "A", "T", VariantType.SNV, null, false, false, null, null, new AnnotationBehavior(true, false, false, true, false, false));
+            var annotatedVariant = new AnnotatedVariant(variant);
+
+            annotatedVariant.SupplementaryAnnotations.Add(new AnnotatedSaDataSource(new SaDataSource("oneKg", "AF1000G", "T", true, false, "0.000599;t", null), "T"));
+            IAnnotatedVariant[] annotatedVariants = { annotatedVariant };
+            var annotatedPosition = new AnnotatedPosition(position, annotatedVariants);
+
+            var converter = new VcfConversion();
+            var observedVcf = converter.Convert(annotatedPosition).Split("\t")[VcfCommon.InfoIndex];
+
+            Assert.Equal("AA=t;AF1000G=0.000599", observedVcf);
         }
 
         [Fact]
         public void Only_allele_specific_entry_for_annotation_not_matched_by_allele_is_output()
         {
-            
+            var vcfFields = "chr1	101	sa123	A	T	.	.	.".Split("\t");
+            var chrom = new Chromosome("chr1", "1", 0);
+            var inforData = new InfoData(null, null, VariantType.SNV, null, null, null, null, null, false, null, null,
+                false, false, "", null, null);
+            var position = new Position(chrom, 101, 101, "A", new[] { "T" }, 100, null, null, null, inforData, vcfFields);
+            var variant = new Variant(chrom, 101, 101, "A", "T", VariantType.SNV, null, false, false, null, null, new AnnotationBehavior(true, false, false, true, false, false));
+            var annotatedVariant = new AnnotatedVariant(variant);
+
+            annotatedVariant.SupplementaryAnnotations.Add(new AnnotatedSaDataSource(new SaDataSource("testSource", "Test", "T", false, true, "pathogenic", null), "T"));
+            annotatedVariant.SupplementaryAnnotations.Add(new AnnotatedSaDataSource(new SaDataSource("testSource", "Test", "G", false, true, "benign", null), "T"));
+            IAnnotatedVariant[] annotatedVariants = { annotatedVariant };
+            var annotatedPosition = new AnnotatedPosition(position, annotatedVariants);
+
+            var converter = new VcfConversion();
+            var observedVcf = converter.Convert(annotatedPosition).Split("\t")[VcfCommon.InfoIndex];
+
+            Assert.Equal("Test=1|pathogenic", observedVcf);
+        }
+
+        [Fact]
+        public void Only_canonical_transcripts_are_reported_in_vcf()
+        {
+            var mockedTranscript1 = new Mock<IAnnotatedTranscript>();
+            mockedTranscript1.Setup(x => x.Transcript.IsCanonical).Returns(true);
+            mockedTranscript1.Setup(x => x.Transcript.Id).Returns(CompactId.Convert("ENST12345"));
+            mockedTranscript1.Setup(x => x.Transcript.Version).Returns(1);
+            mockedTranscript1.Setup(x => x.Transcript.Gene.Symbol).Returns("testGene1");
+            mockedTranscript1.SetupGet(x => x.Consequences)
+                .Returns(new List<ConsequenceTag> {ConsequenceTag.five_prime_UTR_variant});
+
+            var mockedTranscript2 = new Mock<IAnnotatedTranscript>();
+            mockedTranscript2.Setup(x => x.Transcript.IsCanonical).Returns(false);
+            mockedTranscript2.Setup(x => x.Transcript.Id).Returns(CompactId.Convert("ENST23456"));
+            mockedTranscript2.Setup(x => x.Transcript.Version).Returns(2);
+            mockedTranscript2.Setup(x => x.Transcript.Gene.Symbol).Returns("testGene2");
+            mockedTranscript2.SetupGet(x => x.Consequences)
+                .Returns(new List<ConsequenceTag> { ConsequenceTag.missense_variant });
+
+            var mockedTranscript3 = new Mock<IAnnotatedTranscript>();
+            mockedTranscript3.Setup(x => x.Transcript.IsCanonical).Returns(true);
+            mockedTranscript3.Setup(x => x.Transcript.Id).Returns(CompactId.Convert("NM1234.3"));
+            mockedTranscript3.Setup(x => x.Transcript.Version).Returns(3);
+            mockedTranscript3.Setup(x => x.Transcript.Gene.Symbol).Returns("testGene3");
+            mockedTranscript3.SetupGet(x => x.Consequences)
+                .Returns(new List<ConsequenceTag> { ConsequenceTag.missense_variant,ConsequenceTag.splice_region_variant });
+
+
+            var vcfFields = "chr1	101	sa123	A	T	.	.	.".Split("\t");
+            var chrom = new Chromosome("chr1", "1", 0);
+            var inforData = new InfoData(null, null, VariantType.SNV, null, null, null, null, null, false, null, null,
+                false, false, "", null, null);
+            var position = new Position(chrom, 101, 101, "A", new[] { "T" }, 100, null, null, null, inforData, vcfFields);
+            var variant = new Variant(chrom, 101, 101, "A", "T", VariantType.SNV, null, false, false, null, null, new AnnotationBehavior(true, false, false, true, false, false));
+            var annotatedVariant = new AnnotatedVariant(variant);
+            annotatedVariant.EnsemblTranscripts.Add(mockedTranscript1.Object);
+            annotatedVariant.EnsemblTranscripts.Add(mockedTranscript2.Object);
+            annotatedVariant.RefSeqTranscripts.Add(mockedTranscript3.Object);
+
+            IAnnotatedVariant[] annotatedVariants = { annotatedVariant };
+            var annotatedPosition = new AnnotatedPosition(position, annotatedVariants);
+
+            var converter = new VcfConversion();
+            var observedVcf = converter.Convert(annotatedPosition).Split("\t")[VcfCommon.InfoIndex];
+
+            Assert.Equal("CSQT=1|testGene1|ENST12345.1|5_prime_UTR_variant,1|testGene3|.3|missense_variant&splice_region_variant", observedVcf);
+
+        }
+
+        [Fact]
+        public void regulatory_region_is_added()
+        {
+            var mockedRegulatory = new Mock<IAnnotatedRegulatoryRegion>();
+            mockedRegulatory.SetupGet(x => x.Consequences).Returns(new List<ConsequenceTag> { ConsequenceTag.regulatory_region_variant});
+            mockedRegulatory.SetupGet(x => x.RegulatoryRegion.Id).Returns(CompactId.Convert("ENSR12345"));
+
+            var vcfFields = "chr1	101	sa123	A	.	.	.	.	.".Split("\t");
+            var chrom = new Chromosome("chr1", "1", 0);
+            var inforData = new InfoData(null, null, VariantType.SNV, null, null, null, null, null, false, null, null,
+                false, false, "Test=abc", null, null);
+            var position = new Position(chrom, 101, 101, "A", new[] { "." }, 100, null, null, null, inforData, vcfFields);
+            var variant = new Variant(chrom, 101, 101, "A", ".", VariantType.reference, null, false, false, null, null, new AnnotationBehavior(true, false, false, true, false, false));
+            var annotatedVariant = new AnnotatedVariant(variant);
+            annotatedVariant.RegulatoryRegions.Add(mockedRegulatory.Object);
+
+            IAnnotatedVariant[] annotatedVariants = { annotatedVariant };
+            var annotatedPosition = new AnnotatedPosition(position, annotatedVariants);
+
+            var converter = new VcfConversion();
+            var observedVcf = converter.Convert(annotatedPosition).Split("\t")[VcfCommon.InfoIndex];
+
+            Assert.Equal("Test=abc;CSQR=1|ENSR12345|regulatory_region_variant", observedVcf);
         }
 
     }
--- VariantAnnotation/IO/VcfWriter/VcfField.cs
+++ VariantAnnotation/IO/VcfWriter/VcfField.cs
@@ -23,28 +23,6 @@ namespace VariantAnnotation.IO.VcfWriter
             _entries.Add(s);
         }
 
-        /// <summary>
-        /// given a vcf field (like the INFO field), this method returns a dictionary of the keys and values
-        /// </summary>
-        public static Dictionary<string, string> GetKeysAndValues(string vcfField)
-        {
-            var fields = new Dictionary<string, string>();
-
-            foreach (var infoPair in vcfField.Split(';'))
-            {
-                var keyValuePair = infoPair.Split('=');
-
-                // sanity check: make sure we only have one or two entries
-                if (keyValuePair.Length == 0 || keyValuePair.Length > 2)
-                {
-                    throw new NotSupportedException($"Expected one or two entries in the key/value pair, but found {keyValuePair.Length} entries: {infoPair}");
-                }
-
-                fields.Add(keyValuePair[0], keyValuePair.Length == 2 ? keyValuePair[1] : null);
-            }
-
-            return fields;
-        }
 
         /// <summary>
         /// returns a string representation of the VCF ID field
