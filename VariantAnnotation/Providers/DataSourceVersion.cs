a3be40c4879445fb5691b19a5fe4e8be5fb413fd
Michael Stromberg <mstromberg@illumina.com>
2017-11-29 11:23:15 -0800

    Fixed the data source version uniqueness issue.

--- VariantAnnotation/Providers/DataSourceVersion.cs
+++ VariantAnnotation/Providers/DataSourceVersion.cs
@@ -1,91 +1,82 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Text;
 using VariantAnnotation.Interface.IO;
 using VariantAnnotation.Interface.Providers;
 using VariantAnnotation.IO;
+using VariantAnnotation.Utilities;
 
 namespace VariantAnnotation.Providers
 {
-	public sealed class DataSourceVersion : IDataSourceVersion, IEquatable<DataSourceVersion>, ISerializable
-	{
-		public string Name { get; }
-		public string Description { get; }
-		public string Version { get; }
-		public long ReleaseDateTicks { get; }
+    public sealed class DataSourceVersion : IDataSourceVersion, ISerializable
+    {
+        public string Name { get; }
+        public string Description { get; }
+        public string Version { get; }
+        public long ReleaseDateTicks { get; }
 
-		public DataSourceVersion(string name, string version, long releaseDateTicks, string description = null)
-		{
-			Name             = name;
-			Description      = description;
-			Version          = version;
-			Description      = description;
-			ReleaseDateTicks = releaseDateTicks;
-		}
+        public DataSourceVersion(string name, string version, long releaseDateTicks, string description = null)
+        {
+            Name             = name;
+            Description      = description;
+            Version          = version;
+            Description      = description;
+            ReleaseDateTicks = releaseDateTicks;
+        }
 
-	    public static IDataSourceVersion Read(ExtendedBinaryReader reader)
-	    {
-	        var name = reader.ReadAsciiString();
-	        var version = reader.ReadAsciiString();
-	        var releaseDateTicks = reader.ReadOptInt64();
-	        var description = reader.ReadAsciiString();
-	        return new DataSourceVersion(name, version, releaseDateTicks, description);
-	    }
+        public static IDataSourceVersion Read(ExtendedBinaryReader reader)
+        {
+            var name             = reader.ReadAsciiString();
+            var version          = reader.ReadAsciiString();
+            var releaseDateTicks = reader.ReadOptInt64();
+            var description      = reader.ReadAsciiString();
+            return new DataSourceVersion(name, version, releaseDateTicks, description);
+        }
 
         public void Write(IExtendedBinaryWriter writer)
-		{
-			writer.WriteOptAscii(Name);
-			writer.WriteOptAscii(Version);
-			writer.WriteOpt(ReleaseDateTicks);
-			writer.WriteOptAscii(Description);
-		}
-		
-		/// <summary>
-		/// returns the release date
-		/// </summary>
-		private string GetReleaseDate()
-		{
-			return new DateTime(ReleaseDateTicks, DateTimeKind.Utc).ToString("yyyy-MM-dd");
-		}
+        {
+            writer.WriteOptAscii(Name);
+            writer.WriteOptAscii(Version);
+            writer.WriteOpt(ReleaseDateTicks);
+            writer.WriteOptAscii(Description);
+        }
 
-		public override string ToString()
-		{
-			return "dataSource=" + Name + ",version:" + Version + ",release date:" + GetReleaseDate();
-		}
+        private string GetReleaseDate() => Date.GetDate(ReleaseDateTicks);
 
-		/// <summary>
-		/// serializes this object to a textual JSON representation via the
-		/// string builder.
-		/// </summary>
-		public void SerializeJson(StringBuilder sb)
-		{
-			var jsonObject = new JsonObject(sb);
+        public override string ToString() => "dataSource=" + Name + ",version:" + Version + ",release date:" + GetReleaseDate();
 
-			sb.Append(JsonObject.OpenBrace);
-			jsonObject.AddStringValue("name", Name);
-			jsonObject.AddStringValue("version", Version);
-			if (Description != null) jsonObject.AddStringValue("description", Description.Trim());
-			if (ReleaseDateTicks != 0) jsonObject.AddStringValue("releaseDate", GetReleaseDate());
-			sb.Append(JsonObject.CloseBrace);
-		}
+        public void SerializeJson(StringBuilder sb)
+        {
+            var jsonObject = new JsonObject(sb);
 
-	    public bool Equals(DataSourceVersion other)
-	    {
-	        if (ReferenceEquals(null, other)) return false;
-	        if (ReferenceEquals(this, other)) return true;
-	        return string.Equals(Name, other.Name) && string.Equals(Description, other.Description) &&
-	               string.Equals(Version, other.Version) && ReleaseDateTicks == other.ReleaseDateTicks;
-	    }
+            sb.Append(JsonObject.OpenBrace);
+            jsonObject.AddStringValue("name", Name);
+            jsonObject.AddStringValue("version", Version);
+            if (Description != null) jsonObject.AddStringValue("description", Description.Trim());
+            if (ReleaseDateTicks != 0) jsonObject.AddStringValue("releaseDate", GetReleaseDate());
+            sb.Append(JsonObject.CloseBrace);
+        }
+    }
 
-	    public override int GetHashCode()
-	    {
-	        unchecked
-	        {
-	            var hashCode = (Name != null ? Name.GetHashCode() : 0);
-	            hashCode = (hashCode * 397) ^ (Description != null ? Description.GetHashCode() : 0);
-	            hashCode = (hashCode * 397) ^ (Version != null ? Version.GetHashCode() : 0);
-	            hashCode = (hashCode * 397) ^ ReleaseDateTicks.GetHashCode();
-	            return hashCode;
-	        }
-	    }
-	}
+    internal sealed class DataSourceVersionComparer : EqualityComparer<IDataSourceVersion>
+    {
+        public override bool Equals(IDataSourceVersion x, IDataSourceVersion y)
+        {
+            return string.Equals(x.Name, y.Name) &&
+                   string.Equals(x.Description, y.Description) &&
+                   string.Equals(x.Version, y.Version) &&
+                   x.ReleaseDateTicks == y.ReleaseDateTicks;
+        }
+
+        public override int GetHashCode(IDataSourceVersion x)
+        {
+            unchecked
+            {
+                var hashCode = x.Name.GetHashCode();
+                if (x.Description != null) hashCode = (hashCode * 397) ^ x.Description.GetHashCode();
+                if (x.Version != null) hashCode = (hashCode * 397) ^ x.Version.GetHashCode();
+                hashCode = (hashCode * 397) ^ x.ReleaseDateTicks.GetHashCode();
+                return hashCode;
+            }
+        }
+    }
 }
