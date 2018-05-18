ed1cae42cf5f4744ae69ced4f22d0097a6ae4808
Stromberg, Michael <mstromberg@illumina.com>
2018-05-18 09:16:36 -0700

    Added support for the Zstandard dictionary. (#191)

--- UnitTests/Compression/CompressionAlgorithmTests.cs
+++ UnitTests/Compression/CompressionAlgorithmTests.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 using Compression.Algorithms;
 using UnitTests.Compression.FileHandling;
+using UnitTests.TestUtilities;
 using Xunit;
 
 namespace UnitTests.Compression
@@ -9,8 +12,53 @@ namespace UnitTests.Compression
     {
         private const int NumOriginalBytes = 20000;
         private readonly byte[] _originalBytes;
+        private readonly byte[] _dictBuffer;
 
-        public CompressionAlgorithmTests() => _originalBytes = BlockStreamTests.GetRandomBytes(NumOriginalBytes);
+        public CompressionAlgorithmTests()
+        {
+            _originalBytes = BlockStreamTests.GetRandomBytes(NumOriginalBytes);
+            _dictBuffer    = LoadZstandardDictionary();
+        }
+
+        private static byte[] LoadZstandardDictionary() => File.ReadAllBytes(Resources.TopPath("clinvar.dict"));
+
+        [Fact]
+        public void RoundTrip_Zstandard_WithDictionary()
+        {
+            const string expectedString = "\"id\":\"RCV000537631.1\",\"reviewStatus\":\"criteria provided, single submitter\",\"alleleOrigins\":[\"germline\"],\"refAllele\":\"G\",\"altAllele\":\"A\",\"phenotypes\":[\"Immunodeficiency 38 with basal ganglia calcification\"],\"medGenIds\":[\"C4015293\"],\"omimIds\":[\"616126\"],\"orphanetIds\":[\"319563\"],\"significance\":\"benign\",\"lastUpdatedDate\":\"2017-12-27\",\"pubMedIds\":[\"28492532\"]";
+            byte[] expectedBytes        = Encoding.UTF8.GetBytes(expectedString);
+            var zstd                    = new ZstandardDict(17, _dictBuffer);
+
+            int compressedBufferSize    = zstd.GetCompressedBufferBounds(expectedBytes.Length);
+            var observedCompressedBytes = new byte[compressedBufferSize];
+
+            Assert.Throws<InvalidOperationException>(delegate
+            {
+                zstd.Compress(expectedBytes, expectedBytes.Length, null, compressedBufferSize);
+            });
+
+            int numCompressedBytes = zstd.Compress(expectedBytes, expectedBytes.Length, observedCompressedBytes,
+                compressedBufferSize);
+
+            int decompressedBufferSize    = expectedBytes.Length;
+            var observedDecompressedBytes = new byte[decompressedBufferSize];
+
+            Assert.Throws<InvalidOperationException>(delegate
+            {
+                zstd.Decompress(observedCompressedBytes, numCompressedBytes, null, decompressedBufferSize);
+            });
+
+            int numDecompressedBytes = zstd.Decompress(observedCompressedBytes, numCompressedBytes,
+                observedDecompressedBytes, decompressedBufferSize);
+
+            Assert.Equal(expectedBytes.Length, numDecompressedBytes);
+            Assert.Equal(expectedBytes, observedDecompressedBytes);
+
+            Assert.Throws<NotImplementedException>(delegate
+            {
+                zstd.GetDecompressedLength(observedCompressedBytes, numCompressedBytes);
+            });
+        }
 
         [Theory]
         [InlineData(CompressionAlgorithms.QuickLz)]
