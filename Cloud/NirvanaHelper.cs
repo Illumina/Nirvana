f8d632266d4c32a72bdc1b582a11956d5dd5397f
Stromberg, Michael <mstromberg@illumina.com>
2018-10-25 11:50:45 -0700

    Feature/s3 url update (#232)
    * Pointed the S3 URL to the new Olympia bucket
    
    * Removed terminal slash

--- Cloud/NirvanaHelper.cs
+++ Cloud/NirvanaHelper.cs
@@ -7,11 +7,11 @@ namespace Cloud
     public static class NirvanaHelper
     {
         public const int AnnotationTimeOut = 295_000;
-        public const string S3Url = "https://illumina-annotation.s3.amazonaws.com/";
+        public const string S3Url = "https://ilmn-nirvana.s3-us-west-2.amazonaws.com/";
         public const string ProjectName = "RiverRun";
-        public const string S3CacheFoler = S3Url + "Cache/26/";
+        public const string S3CacheFoler = S3Url + "ab0cf104f39708eabd07b8cb67e149ba-Cache/26/";
         public const string DefaultCacheSource = "Both";
-        public const string S3RefPrefix = S3Url + "References/5/Homo_sapiens.";
+        public const string S3RefPrefix = S3Url + "d95867deadfe690e40f42068d6b59df8-References/5/Homo_sapiens.";
 
         public const string RefSuffix = ".Nirvana.dat";
         public const string JsonSuffix = ".json.gz";
