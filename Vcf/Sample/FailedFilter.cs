4181c095f1f6498742eedd17c84e120ed0e10f33
Roy, Rajat <rroy1@illumina.com>
2018-12-18 09:21:58 -0800

    adding check for missing failed filter fields (#254)

--- Vcf/Sample/FailedFilter.cs
+++ Vcf/Sample/FailedFilter.cs
@@ -5,6 +5,8 @@
         public static bool GetFailedFilter(IntermediateSampleFields intermediateSampleFields)
         {
             if (intermediateSampleFields.FormatIndices.FT == null) return false;
+            if (intermediateSampleFields.FormatIndices.FT.Value >=
+                intermediateSampleFields.SampleColumns.Length) return false;
             string filterValue = intermediateSampleFields.SampleColumns[intermediateSampleFields.FormatIndices.FT.Value];
             return filterValue != "PASS" && filterValue != ".";
         }
