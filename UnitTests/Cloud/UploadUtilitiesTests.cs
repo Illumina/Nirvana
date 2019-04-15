7c131d4704be2c518691c50ede01fe9fc7631a77
Roy, Rajat <rroy1@illumina.com>
2019-04-15 16:06:38 -0700

    Bugfix/from april 2019 (#318)
    * Restored develop (#312)
    
    * Bugfix/dev merge candidate (#316)
    
    * Restored develop
    
    * Feature/standardize error message 3777 (#309)
    
    * Add ErrorCategory enum
    
    * Refactor ExceptionUtilities class
    
    * Update NuGet packages
    
    * reorder ErrorCategory
    
    * Remove unnecessary error category
    
    * Fix unit tests
    
    * Resolve SonarQube issues
    
    * Update NuGet packages
    
    * Update the message for VCF genome assembly error
    
    * Adding cache conversion script (#314)
    
    * Remove redundent type and properties json schema keys (#311)
    
    * Fixed the issue (#315)
    
    * Fixed the issue
    
    * Use empty array instead of null
    
    * retry when uploading is failed (#317)

--- UnitTests/Cloud/UploadUtilitiesTests.cs
+++ UnitTests/Cloud/UploadUtilitiesTests.cs
@@ -1,5 +1,10 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Net;
 using System.Security.Cryptography;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using System.Threading.Tasks;
 using Amazon.S3.Model;
 using Cloud;
 using IO;
@@ -12,7 +17,8 @@ namespace UnitTests.Cloud
     public sealed class UploadUtilitiesTests
     {
         private readonly FileMetadata _metadata = new FileMetadata(new byte[] { 0, 1, 2, 3, 4, 5, 6 }, 1);
-        private readonly string _filePath       = Resources.TopPath("clinvar.dict");
+        private readonly string _filePath = Resources.TopPath("clinvar.dict");
+        private readonly AesCryptoServiceProvider _aes = new AesCryptoServiceProvider();
 
         private static Mock<IS3Client> GetS3ClientMock()
         {
@@ -21,33 +27,68 @@ namespace UnitTests.Cloud
             return s3ClientMock;
         }
 
-        [Fact]
-        public void DecryptUpload_AsExpected()
+        private static Mock<IS3Client> GetS3ClientMockAlwaysFail()
         {
-            Mock<IS3Client> s3ClientMock = GetS3ClientMock();
+            var s3ClientMock = new Mock<IS3Client>();
+            s3ClientMock.Setup(x => x.PutObjectAsync(It.IsAny<PutObjectRequest>())).ThrowsAsync(new WebException());
+            return s3ClientMock;
+        }
 
-            using (var aes = new AesCryptoServiceProvider())
-            {
-                var s3Client = s3ClientMock.Object;
-                s3Client.DecryptUpload("bucket", "bob.json.gz", _filePath, aes, _metadata);
-            }
+        private static Mock<IS3Client> GetS3ClientMockCanWorkAfterRetries()
+        {
+            var s3ClientMock = new Mock<IS3Client>();
+            s3ClientMock.SetupSequence(x => x.PutObjectAsync(It.IsAny<PutObjectRequest>()))
+                .ThrowsAsync(new WebException())
+                .ThrowsAsync(new WebException())
+                .ReturnsAsync(new PutObjectResponse())
+                .ThrowsAsync(new WebException());
 
+            return s3ClientMock;
+        }
+
+        [Fact]
+        public void TryDecryptUpload_AsExpected()
+        {
+            var s3ClientMock = GetS3ClientMock();
+            Assert.True(s3ClientMock.Object.TryDecryptUpload("bucket", "bob.json.gz", _filePath, _aes, _metadata));
             s3ClientMock.Verify(x => x.PutObjectAsync(It.IsAny<PutObjectRequest>()), Times.Once);
         }
 
         [Fact]
-        public void DecryptUpload_FileNotFound()
+        public void TryDecryptUpload_FileNotFound()
         {
-            Assert.Throws<FileNotFoundException>(delegate
-            {
-                Mock<IS3Client> s3ClientMock = GetS3ClientMock();
+            var s3ClientMock = GetS3ClientMock();
+            Assert.False(s3ClientMock.Object.TryDecryptUpload("bucket", "bob.json.gz", "bob123", _aes, _metadata));
+            s3ClientMock.Verify(x => x.PutObjectAsync(It.IsAny<PutObjectRequest>()), Times.Never);
+        }
 
-                using (var aes = new AesCryptoServiceProvider())
-                {
-                    var s3Client = s3ClientMock.Object;
-                    s3Client.DecryptUpload("bucket", "bob.json.gz", "bob123", aes, _metadata);
-                }
-            });
+        [Fact]
+        public void DecryptUpload_OnlyPutOnceWhenSuccess()
+        {
+            var s3ClientMock = GetS3ClientMock();
+
+            s3ClientMock.Object.DecryptUpload("bucket", "bob.json.gz", _filePath, _aes, _metadata, 1);
+            s3ClientMock.Verify(x => x.PutObjectAsync(It.IsAny<PutObjectRequest>()), Times.Once);
+        }
+
+        [Fact]
+        public void DecryptUpload_SuccessWithRetries()
+        {
+            var s3ClientMock = GetS3ClientMockCanWorkAfterRetries();
+
+            s3ClientMock.Object.DecryptUpload("bucket", "bob.json.gz", _filePath, _aes, _metadata, 1);
+            s3ClientMock.Verify(x => x.PutObjectAsync(It.IsAny<PutObjectRequest>()), Times.Exactly(3));
+        }
+
+        [Fact]
+        public void DecryptUpload_TimeOutWhenFail()
+        {
+            var timeOut = TimeSpan.FromMilliseconds(500);
+            var s3ClientMockAlwaysFail = GetS3ClientMockAlwaysFail();
+            var failTask = Task.Run(() => s3ClientMockAlwaysFail.Object.DecryptUpload("bucket", "bob.json.gz", _filePath, _aes, _metadata, 1));
+      
+            Assert.False(Task.WaitAll(new[] { failTask }, timeOut));
+            s3ClientMockAlwaysFail.Verify(x => x.PutObjectAsync(It.IsAny<PutObjectRequest>()), Times.AtLeast(2));
         }
     }
 }
