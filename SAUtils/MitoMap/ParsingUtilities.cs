7f381c9fdd27803f5b336264f97b57138fd3898d
Kang, Shuli <skang@illumina.com>
2020-09-14 11:25:50 -0700

    Fix the bug in merge mitomap records and add unit test for internal r… (#532)
    * Fix the bug in merge mitomap records and add unit test for internal reference ID exaction
    
    * Remove duplicated PMIDs
    
    Co-authored-by: Stromberg, Michael <mstromberg@illumina.com>

--- SAUtils/MitoMap/ParsingUtilities.cs
+++ SAUtils/MitoMap/ParsingUtilities.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 
 namespace SAUtils.MitoMap
 {
@@ -10,17 +11,8 @@ namespace SAUtils.MitoMap
         public static List<string> GetPubMedIds(string field, MitoMapInputDb mitoMapInputDb)
         {
             if (field == "0") return default;
-            //"?refs=4,140,189,91687,91737&title="
-            const string leadingString = "refs=";
-            const string trailingString = "&title=";
-            var leadingStringIndex = field.IndexOf(leadingString, StringComparison.Ordinal);
-            var trailingStringIndex = field.IndexOf(trailingString, StringComparison.Ordinal);
-            var startIndex = leadingStringIndex + leadingString.Length;
-            var idStringLength = trailingStringIndex - startIndex;
-            if (leadingStringIndex == -1 || trailingStringIndex == -1 || idStringLength == 0)
-                throw new InvalidDataException($"Failed to extract reference IDs from {field}");
 
-            var internalIds = field.Substring(startIndex, idStringLength).Split(',');
+            var internalIds = ExtractInternalIds(field);
             var pubMedIds = new List<string>();
             foreach (string internalId in internalIds)
             {
@@ -32,7 +24,22 @@ namespace SAUtils.MitoMap
                     throw new InvalidDataException($"Can't find PubMedID corresponding to internal reference ID {internalId} when parsing {field}");
             }
 
-            return pubMedIds;
+            return pubMedIds.Distinct().ToList();
+        }
+
+        public static string[] ExtractInternalIds(string field)
+        {
+            //"?refs=4,140,189,91687,91737&title="
+            const string leadingString = "refs=";
+            const string trailingString = "&title=";
+            var leadingStringIndex = field.IndexOf(leadingString, StringComparison.Ordinal);
+            var trailingStringIndex = field.IndexOf(trailingString, StringComparison.Ordinal);
+            var startIndex = leadingStringIndex + leadingString.Length;
+            var idStringLength = trailingStringIndex - startIndex;
+            if (leadingStringIndex == -1 || trailingStringIndex == -1 || idStringLength == 0)
+                throw new InvalidDataException($"Failed to extract reference IDs from {field}");
+
+            return field.Substring(startIndex, idStringLength).Split(',');
         }
     }
 }
--- UnitTests/SAUtils/MitoMap/ParsingUtilitiesTests.cs
+++ UnitTests/SAUtils/MitoMap/ParsingUtilitiesTests.cs
@@ -12,16 +12,25 @@ namespace UnitTests.SAUtils.MitoMap
             {"2", "102"},
             {"13", "103"},
             {"4100", "104"},
-            {"5678", "105"}
+            {"5678", "105"},
+            {"23202", "105"}
         });
 
         [Theory]
         [InlineData("<a href='/cgi-bin/print_ref_list?refs=4100&title=Control+Polymorphism+T-A+at+14' target='_blank'>1</a>", "104")]
-        [InlineData("<a href='/cgi-bin/print_ref_list?refs=1,13,5678&title=Mutation+T-C+at+15784' target='_blank'>3</a>", "101,103,105")]
+        [InlineData("<a href='/cgi-bin/print_ref_list?refs=1,13,5678,23202&title=Mutation+T-C+at+15784' target='_blank'>3</a>", "101,103,105")]
         [InlineData("<a href='/cgi-bin/print_ref_list?refs=1,2,13,4100&title=Simple+Insertion+%2B266+308-573+D-Loop+region+573+D-Loop+region+D%2C+7%2F7+25' target='_blank'>4</a>", "101,102,103,104")]
         public void GetPubMedIds_AsExpected(string field, string pubmedIds)
         {
             Assert.Equal(string.Join(',', ParsingUtilities.GetPubMedIds(field, MitoMapInputDb)), pubmedIds);
+        }
+
+        [Theory]
+        [InlineData("<a href='/cgi-bin/print_ref_list?refs=1,2,13,4100&title=Simple+Insertion+%2B266+308-573+D-Loop+region+573+D-Loop+region+D%2C+7%2F7+25' target='_blank'>4</a>", "1,2,13,4100")]
+        [InlineData("<a href='/cgi-bin/print_ref_list?refs=45,247,280,303,312,330,332,394,396,541,3311,3370,3427,3569,3584,3732,3943,4287,4946,5113,5329,5348,5451,5452,5628,6169,6221,6228,6421,6490,6531,6603,6623,7647,7695,8405,8492,20372,20373,90237,90296,90302,90311,90331,90333,90365,90377,90453,90468,90477,90483,90534,90572,90627,90764,90809,90812,90835,90903,90904,90941,90963,91015,91020,91030,91041,91056,91068,91077,91081,91094,91099,91206,91214,91215,91221,91227,91228,91256,91269,91311,91318,91394,91420,91431,91514,91687,91737,91775,91789,91830,91958,91999,92063,92082,92100,99016&title=Coding+Polymorphism+G-A+at+3010' target='_blank'>97</a>", "45,247,280,303,312,330,332,394,396,541,3311,3370,3427,3569,3584,3732,3943,4287,4946,5113,5329,5348,5451,5452,5628,6169,6221,6228,6421,6490,6531,6603,6623,7647,7695,8405,8492,20372,20373,90237,90296,90302,90311,90331,90333,90365,90377,90453,90468,90477,90483,90534,90572,90627,90764,90809,90812,90835,90903,90904,90941,90963,91015,91020,91030,91041,91056,91068,91077,91081,91094,91099,91206,91214,91215,91221,91227,91228,91256,91269,91311,91318,91394,91420,91431,91514,91687,91737,91775,91789,91830,91958,91999,92063,92082,92100,99016")]
+        public void ExtractInternalIds_AsExpected(string field, string internalIds)
+        {
+            Assert.Equal(string.Join(',', ParsingUtilities.ExtractInternalIds(field)), internalIds);
         }
     }
 }
