f21e1f2911346026e545c7bcc368026bc2f6fef4
Stromberg, Michael <mstromberg@illumina.com>
2021-04-13 09:54:54 -0700

    Updated HGVS g. notation for MT variants to use m. notation. (#584)
    Co-authored-by: Roy, Rajat <rroy1@illumina.com>

--- UnitTests/VariantAnnotation/AnnotatedPositions/HgvsgNotationTests.cs
+++ UnitTests/VariantAnnotation/AnnotatedPositions/HgvsgNotationTests.cs
@@ -1,6 +1,7 @@
 ﻿using Genome;
 using Intervals;
 using UnitTests.TestDataStructures;
+using UnitTests.TestUtilities;
 using VariantAnnotation.AnnotatedPositions;
 using Variants;
 using Xunit;
@@ -9,23 +10,23 @@ namespace UnitTests.VariantAnnotation.AnnotatedPositions
 {
     public sealed class HgvsgNotationTests
     {
-        private static readonly ISequence SimpleSequence = new SimpleSequence("ATCGGTGCTGACGATACCTGACGTAAGTA");
-        private readonly IInterval _referenceInterval    = new Interval(0, SimpleSequence.Length);
-        private const string RefSeqAccession             = "NC_012920.1";
+        private static readonly ISequence SimpleSequence     = new SimpleSequence("ATCGGTGCTGACGATACCTGACGTAAGTA");
+        private readonly        IInterval _referenceInterval = new Interval(0, SimpleSequence.Length);
+        private const           string    RefSeqAccession    = "NC_012920.1";
 
         [Theory]
-        [InlineData(5, 5, "G", "T", VariantType.SNV, "NC_012920.1:g.5G>T")]
-        [InlineData(5, 5, "G", "G", VariantType.SNV, "NC_012920.1:g.5=")]
-        [InlineData(5, 7, "GTG", "", VariantType.deletion, "NC_012920.1:g.5_7del")]
-        [InlineData(10, 12, "GAC", "", VariantType.deletion, "NC_012920.1:g.12_14del")]
-        [InlineData(16, 15, "", "GATA", VariantType.insertion, "NC_012920.1:g.15_16insGATA")]
-        [InlineData(19, 22, "TGAC", "GTCA", VariantType.MNV, "NC_012920.1:g.19_22invTGAC")]
-        [InlineData(10, 9, "", "GAC", VariantType.insertion, "NC_012920.1:g.12_14dupCGA")]
-        public void GetNotation_MT(int start,int end,string referenceAllele,string altAllele, VariantType type, string expectedHgvsg)
+        [InlineData(5, 5, "G", "T", VariantType.SNV, "NC_012920.1:m.5G>T")]
+        [InlineData(5, 5, "G", "G", VariantType.SNV, "NC_012920.1:m.5=")]
+        [InlineData(5, 7, "GTG", "", VariantType.deletion, "NC_012920.1:m.5_7del")]
+        [InlineData(10, 12, "GAC", "", VariantType.deletion, "NC_012920.1:m.12_14del")]
+        [InlineData(16, 15, "", "GATA", VariantType.insertion, "NC_012920.1:m.15_16insGATA")]
+        [InlineData(19, 22, "TGAC", "GTCA", VariantType.MNV, "NC_012920.1:m.19_22invTGAC")]
+        [InlineData(10, 9, "", "GAC", VariantType.insertion, "NC_012920.1:m.12_14dupCGA")]
+        public void GetNotation_MT(int start, int end, string referenceAllele, string altAllele, VariantType type, string expectedHgvs)
         {
-            var simpleVariant = new SimpleVariant(null,start,end,referenceAllele,altAllele,type);
-            var observedHgvsg = HgvsgNotation.GetNotation(RefSeqAccession, simpleVariant, SimpleSequence, _referenceInterval);
-            Assert.Equal(expectedHgvsg,observedHgvsg);
+            var    simpleVariant = new SimpleVariant(ChromosomeUtilities.ChrM, start, end, referenceAllele, altAllele, type);
+            string actualHgvs    = HgvsgNotation.GetNotation(RefSeqAccession, simpleVariant, SimpleSequence, _referenceInterval);
+            Assert.Equal(expectedHgvs, actualHgvs);
         }
     }
 }
