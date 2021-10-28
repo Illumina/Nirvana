844800df244dfa1d04f5d1b9c1502a9b60ac7c43
Henrik Seidel <henrik.seidel@bayer.com>
2021-10-28 18:46:55 +0200

    Updated CosmicItemTests

--- UnitTests/SAUtils/InputFileParsers/CosmicItemTests.cs
+++ UnitTests/SAUtils/InputFileParsers/CosmicItemTests.cs
@@ -12,8 +12,8 @@ namespace UnitTests.SAUtils.InputFileParsers
         {
             var cosmicItem = new CosmicItem(ChromosomeUtilities.Chr1, 100, "rs101", "A", "C", "GENE0", new HashSet<CosmicItem.CosmicStudy>
             {
-                new CosmicItem.CosmicStudy("100", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 1"}),
-                new CosmicItem.CosmicStudy("100", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 1"})
+                new CosmicItem.CosmicStudy("100", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 1"}, new[]{"tier 0", "tier 1"}),
+                new CosmicItem.CosmicStudy("100", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 1"}, new[]{"tier 0", "tier 1"})
             }, 1);
 
             var counts = cosmicItem.GetTissueCounts();
@@ -27,8 +27,8 @@ namespace UnitTests.SAUtils.InputFileParsers
         {
             var cosmicItem = new CosmicItem(ChromosomeUtilities.Chr1, 100, "rs101", "A", "C", "GENE0", new HashSet<CosmicItem.CosmicStudy>
             {
-                new CosmicItem.CosmicStudy("100", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 2"}),
-                new CosmicItem.CosmicStudy("110", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 1"})
+                new CosmicItem.CosmicStudy("100", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 1"}, new[]{"tier 0", "tier 1"}),
+                new CosmicItem.CosmicStudy("110", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 2"}, new[]{"tier 0", "tier 1"})
             }, 1);
 
             var counts = cosmicItem.GetTissueCounts();
@@ -43,8 +43,8 @@ namespace UnitTests.SAUtils.InputFileParsers
         {
             var cosmicItem = new CosmicItem(ChromosomeUtilities.Chr1, 100, "rs101", "A", "C", "GENE0", new HashSet<CosmicItem.CosmicStudy>
             {
-                new CosmicItem.CosmicStudy("100", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 1"}),
-                new CosmicItem.CosmicStudy("100", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 1"})
+                new CosmicItem.CosmicStudy("100", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 1"}, new[]{"tier 0", "tier 1"}),
+                new CosmicItem.CosmicStudy("100", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 1"}, new[]{"tier 0", "tier 1"})
             }, 1);
 
             var cancerTypeCounts = cosmicItem.GetCancerTypeCounts();
@@ -59,8 +59,8 @@ namespace UnitTests.SAUtils.InputFileParsers
         {
             var cosmicItem = new CosmicItem(ChromosomeUtilities.Chr1, 100, "rs101", "A", "C", "GENE0", new HashSet<CosmicItem.CosmicStudy>
             {
-                new CosmicItem.CosmicStudy("100", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 1"}),
-                new CosmicItem.CosmicStudy("101", new []{"primary histology 0", "histology subtype 2"}, new []{"primarySite 0", "site subtype 1"})
+                new CosmicItem.CosmicStudy("100", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 1"}, new[]{"tier 0", "tier 1"}),
+                new CosmicItem.CosmicStudy("101", new []{"primary histology 0", "histology subtype 2"}, new []{"primarySite 0", "site subtype 1"}, new[]{"tier 0", "tier 1"})
             }, 1);
 
             var cancerTypeCounts = cosmicItem.GetCancerTypeCounts();
@@ -70,16 +70,47 @@ namespace UnitTests.SAUtils.InputFileParsers
             Assert.Equal(1, cancerTypeCounts["histology subtype 2"]);
         }
 
+                public void GetTierCount_same_study()
+        {
+            var cosmicItem = new CosmicItem(ChromosomeUtilities.Chr1, 100, "rs101", "A", "C", "GENE0", new HashSet<CosmicItem.CosmicStudy>
+            {
+                new CosmicItem.CosmicStudy("100", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 1"}, new[]{"tier 0", "tier 1"}),
+                new CosmicItem.CosmicStudy("100", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 1"}, new[]{"tier 0", "tier 1"})
+            }, 1);
+
+            var tierCounts = cosmicItem.GetTierCounts();
+            Assert.Equal(2, tierCounts.Count);
+            Assert.Equal(1, tierCounts["tier 0"]);
+            Assert.Equal(1, tierCounts["tier 1"]);
+        }
+
+
+        [Fact]
+        public void GetTierCount_different_studies()
+        {
+            var cosmicItem = new CosmicItem(ChromosomeUtilities.Chr1, 100, "rs101", "A", "C", "GENE0", new HashSet<CosmicItem.CosmicStudy>
+            {
+                new CosmicItem.CosmicStudy("100", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 1"}, new[]{"tier 0", "tier 1"}),
+                new CosmicItem.CosmicStudy("101", new []{"primary histology 0", "histology subtype 2"}, new []{"primarySite 0", "site subtype 1"}, new[]{"tier 0", "tier 2"})
+            }, 1);
+
+            var tierCounts = cosmicItem.GetTierCounts();
+            Assert.Equal(3, tierCounts.Count);
+            Assert.Equal(2, tierCounts["tier 0"]);
+            Assert.Equal(1, tierCounts["tier 1"]);
+            Assert.Equal(1, tierCounts["tier 2"]);
+        }
+
         [Fact]
         public void GetJsonString()
         {
             var cosmicItem = new CosmicItem(ChromosomeUtilities.Chr1, 100, "rs101", "A", "C", "GENE0", new HashSet<CosmicItem.CosmicStudy>
             {
-                new CosmicItem.CosmicStudy("100", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 1"}),
-                new CosmicItem.CosmicStudy("101", new []{"primary histology 0", "histology subtype 2"}, new []{"primarySite 0", "site subtype 1"})
+                new CosmicItem.CosmicStudy("100", new []{"primary histology 0", "histology subtype 1"}, new []{"primarySite 0", "site subtype 1"}, new[]{"tier 0", "tier 1"}),
+                new CosmicItem.CosmicStudy("101", new []{"primary histology 0", "histology subtype 2"}, new []{"primarySite 0", "site subtype 1"}, new[]{"tier 0", "tier 1"})
             }, 1);
 
-            Assert.Equal("\"id\":\"rs101\",\"refAllele\":\"A\",\"altAllele\":\"C\",\"gene\":\"GENE0\",\"sampleCount\":1,\"cancerTypesAndCounts\":[{\"cancerType\":\"primary histology 0\",\"count\":2},{\"cancerType\":\"histology subtype 1\",\"count\":1},{\"cancerType\":\"histology subtype 2\",\"count\":1}],\"cancerSitesAndCounts\":[{\"cancerSite\":\"primarySite 0\",\"count\":2},{\"cancerSite\":\"site subtype 1\",\"count\":2}]", cosmicItem.GetJsonString());
+            Assert.Equal("\"id\":\"rs101\",\"refAllele\":\"A\",\"altAllele\":\"C\",\"gene\":\"GENE0\",\"sampleCount\":1,\"cancerTypesAndCounts\":[{\"cancerType\":\"primary histology 0\",\"count\":2},{\"cancerType\":\"histology subtype 1\",\"count\":1},{\"cancerType\":\"histology subtype 2\",\"count\":1}],\"cancerSitesAndCounts\":[{\"cancerSite\":\"primarySite 0\",\"count\":2},{\"cancerSite\":\"site subtype 1\",\"count\":2}],\"tiersAndCounts\":[{\"tier\":\"tier 0\",\"count\":2},{\"tier\":\"tier 1\",\"count\":2}]", cosmicItem.GetJsonString());
         }
 
     }
