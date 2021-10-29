6b71b5ade98165b0bf5fce3cdd3c1db40c46a274
Henrik Seidel <henrik.seidel@bayer.com>
2021-10-29 11:31:57 +0200

    Added unit tests for CosmicItem.CosmicTumor.Equals

--- UnitTests/SAUtils/InputFileParsers/CosmicItemTests.cs
+++ UnitTests/SAUtils/InputFileParsers/CosmicItemTests.cs
@@ -8,6 +8,47 @@ namespace UnitTests.SAUtils.InputFileParsers
     public sealed class CosmicItemTests
     {
         [Fact]
+        public void Tumors_Equal()
+        {
+            var tumor01 = new CosmicItem.CosmicTumor("100", "primary histology 0", "primarySite 0", "tier 0");
+            var tumor02 = new CosmicItem.CosmicTumor("100", "primary histology 0", "primarySite 0", "tier 0");
+            var tumor03 = new CosmicItem.CosmicTumor("101", "primary histology 0", "primarySite 0", "tier 0");
+
+            var tumor04 = new CosmicItem.CosmicTumor("100", null, null, null);
+            var tumor05 = new CosmicItem.CosmicTumor("100", null, null, null);
+            var tumor06 = new CosmicItem.CosmicTumor("101", null, null, null);
+
+            var tumor07 = new CosmicItem.CosmicTumor("100", "primary histology 0", "primarySite 0", "tier 0");
+            var tumor08 = new CosmicItem.CosmicTumor("100", "primary histology 1", "primarySite 0", "tier 0");
+            var tumor09 = new CosmicItem.CosmicTumor("100", null,                  "primarySite 0", "tier 0");
+
+            var tumor10 = new CosmicItem.CosmicTumor("100", "primary histology 0", "primarySite 0", "tier 0");
+            var tumor11 = new CosmicItem.CosmicTumor("100", "primary histology 0", "primarySite 1", "tier 0");
+            var tumor12 = new CosmicItem.CosmicTumor("100", "primary histology 0", null,            "tier 0");
+
+            var tumor13 = new CosmicItem.CosmicTumor("100", "primary histology 0", "primarySite 0", "tier 0");
+            var tumor14 = new CosmicItem.CosmicTumor("100", "primary histology 0", "primarySite 0", "tier 1");
+            var tumor15 = new CosmicItem.CosmicTumor("100", "primary histology 0", "primarySite 0", null    );
+
+
+            Assert.True(tumor01.Equals(tumor02));
+            Assert.True(tumor04.Equals(tumor05));
+
+            Assert.False(tumor01.Equals(tumor03));
+            Assert.False(tumor01.Equals(tumor04));
+            Assert.False(tumor05.Equals(tumor06));
+            Assert.False(tumor07.Equals(tumor08));
+            Assert.False(tumor07.Equals(tumor09));
+            Assert.False(tumor08.Equals(tumor09));
+            Assert.False(tumor10.Equals(tumor11));
+            Assert.False(tumor10.Equals(tumor12));
+            Assert.False(tumor11.Equals(tumor12));
+            Assert.False(tumor13.Equals(tumor14));
+            Assert.False(tumor13.Equals(tumor15));
+            Assert.False(tumor14.Equals(tumor15));
+        }
+
+        [Fact]
         public void GetTissueCount_same_tumor()
         {
             var cosmicItem = new CosmicItem(ChromosomeUtilities.Chr1, 100, "rs101", "A", "C", "GENE0", new HashSet<CosmicItem.CosmicTumor>
